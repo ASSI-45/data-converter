d02cd524b6e2fa3cbb78e54ce74eeeccd9448655
ASSI-45 <zencolorbrush@gmail.com>
2024-03-01 18:48:33 +0100

    added more code comments

--- main.py
+++ main.py
@@ -11,10 +11,10 @@
 
 def make_dict(path="./lorum.txt"):
     with open(path, mode="r") as fille:
-        arr = fille.read()
-        arr_new = arr.translate({ord(','):None}).translate({ord('.'):None})
+        string = fille.read()
+        new_stiring = string.translate({ord(','):None}).translate({ord('.'):None})
         result = {} 
-        for i, word in enumerate(arr_new.split()):
+        for i, word in enumerate(new_stiring.split()):
             result[i] = word.title() 
             print(f"{i + 1} : {word.title()}") 
         # print(result)
@@ -28,17 +28,19 @@ def make_dict(path="./lorum.txt"):
 
 def make_dict_and_write(path="./random.txt", path_w="./new.txt"):
     with open(path, mode="r") as file:
-        arr = file.read()
-        arr_new = arr.translate({ord(','):None}).translate({ord('.'):None})
-        result = {0 : ""} 
-        print(arr_new)
+        string = file.read()  # puts the fille into a string type 
+        # cleans it up 
+        new_string = string.translate({ord(','):None}).translate({ord('.'):None})
+        result = {0 : ""} # creates a dictonary 
+        print(new_string)
         try:
             with open(path_w, mode="r") as test_read:
-                if (len(test_read.read()) > 2):
+                if (len(test_read.read()) > 2):  # testing if the fille is empty
                     q = input("-- The fille isn't empy do you wish to overwerite? yes or no: ")
                     if (q == 'yes'):
-                        open(path_w, mode="w").close()
-                        for i, word in enumerate(arr_new.split()):
+                        open(path_w, mode="w").close()  # opens the fille to delete contents
+                        # will write to the fille 
+                        for i, word in enumerate(new_string.split()):
                             with open(path_w, mode="a") as write_file:
                                 write_file.write(f"{i} : {word.title()},\n")
                                 print(f"{i + 1} : {word.title()}")
@@ -47,8 +49,10 @@ def make_dict_and_write(path="./random.txt", path_w="./new.txt"):
                         print("--! not writing anything")
                         return
                 else:
+                    # this will just append the fille and append to it.
                     print("printing to fille")
-                    for i, word in enumerate(arr_new.split()):
+                    for i, word in enumerate(new_string.split()):
+                        # will write to the fille
                         with open(path_w, mode="a") as write_file:
                             write_file.write(f"{i} : {word.title()},\n")
                             print(f"{i + 1} : {word.title()}")
@@ -57,22 +61,15 @@ def make_dict_and_write(path="./random.txt", path_w="./new.txt"):
             print("an error has accourd")
     """
     This function will take an fille.
-    And write it out out as an Dictonary.
+    And write it out out as an Dictonary. And also write it out as fille.
+    Defualt input filles: lorum.txt and random.txt
+    Default output fille: new.txt
     """
-    # return result
 
 
-# def print_dict(self):
-    # for i, word in result:
-        # print(f"{i} : {word.title()}")
-
-
-def main() -> None:
+def main() -> None:  # main function
     make_dict_and_write()
-    # make_dict()
-    # test = make_dict("./random.txt", "./new.txt")
-    # print(test)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # intry point if the fille is ran
     main()
