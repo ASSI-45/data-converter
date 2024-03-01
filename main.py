91799dae56235023f428c030cef59a2aa58f223f
ASSI-45 <zencolorbrush@gmail.com>
2024-03-01 18:28:07 +0100

    error handeling and ask for overwrite permission if needed and just works in my test cases

--- main.py
+++ main.py
@@ -30,20 +30,36 @@ def make_dict_and_write(path="./random.txt", path_w="./new.txt"):
     with open(path, mode="r") as file:
         arr = file.read()
         arr_new = arr.translate({ord(','):None}).translate({ord('.'):None})
-        result = {} 
-        for i, word in enumerate(arr_new.split()):
-            # result[i + 1].append(word.title())
-            # print('--', result[:]) 
-            with open(path_w, mode="w") as write_file:
-                write_file.write("stuff")
-            print(f"{i + 1} : {word.title()}") 
-        # print(result)
-
+        result = {0 : ""} 
+        print(arr_new)
+        try:
+            with open(path_w, mode="r") as test_read:
+                if (len(test_read.read()) > 2):
+                    q = input("-- The fille isn't empy do you wish to overwerite? yes or no: ")
+                    if (q == 'yes'):
+                        open(path_w, mode="w").close()
+                        for i, word in enumerate(arr_new.split()):
+                            with open(path_w, mode="a") as write_file:
+                                write_file.write(f"{i} : {word.title()},\n")
+                                print(f"{i + 1} : {word.title()}")
+                        return result
+                    else:
+                        print("--! not writing anything")
+                        return
+                else:
+                    print("printing to fille")
+                    for i, word in enumerate(arr_new.split()):
+                        with open(path_w, mode="a") as write_file:
+                            write_file.write(f"{i} : {word.title()},\n")
+                            print(f"{i + 1} : {word.title()}")
+                    return result
+        except:
+            print("an error has accourd")
     """
     This function will take an fille.
     And write it out out as an Dictonary.
     """
-    return result
+    # return result
 
 
 # def print_dict(self):
